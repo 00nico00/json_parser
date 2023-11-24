18d77665f00be3d29ee4399b1c5c4281f6b7966a
nico233 <1601550204@qq.com>
2023-11-24 17:27:44 +0800

    Add test for parsing null, true, false

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,32 @@
-//
-// Created by Shay on 2023/11/24.
-//
+#include <gtest/gtest.h>
+
+#include "JsonParser.hpp"
+
+using namespace Nico233;
+
+TEST(test_parse_null, parse_null) {
+    json_value v{};
+    v.type = json_type::True;
+    EXPECT_EQ(parse_code::parse_ok, v.parse("null"));
+    EXPECT_EQ(json_type::Null, v.type);
+    EXPECT_EQ(parse_code::root_not_singular, v.parse("null x"));
+}
+
+TEST(test_parse_true, parse_true) {
+    json_value v{};
+    v.type = json_type::Null;
+    EXPECT_EQ(parse_code::parse_ok, v.parse("true"));
+    EXPECT_EQ(json_type::True, v.type);
+}
+
+TEST(test_parse_false, parse_false) {
+    json_value v{};
+    v.type = json_type::Null;
+    EXPECT_EQ(parse_code::parse_ok, v.parse("false"));
+    EXPECT_EQ(json_type::False, v.type);
+}
+
+int main() {
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
