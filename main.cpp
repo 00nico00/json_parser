ed2db7214ea05e3f158f7ecd35d6f19c7d3745d3
nico233 <1601550204@qq.com>
2023-11-24 15:16:29 +0800

    Add gtest

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
+enum class json_type { Null, False, True, Number, String, Array, Object };
+
+enum class parse_error {
+    expect_value,       // 一个 JSON 只含有空白
+    invalid_value,      // 值不是那三种字面值
+    root_not_singular   // 一个值之后，在空白之后还有其他字符
+};
+
+struct json_value {
+    json_type type;
+
+
+    // function
+    int parse(const std::string& value) {
+
+    }
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
